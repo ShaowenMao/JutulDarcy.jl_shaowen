34fb7d151df8bc0ecad48f5de22b2a1889800886
Shaowen Mao <71668992+ShaowenMao@users.noreply.github.com>
2025-08-12 16:44:55 -0400

    Update predict_input.jl
    add different commands for desktop and Dell laptop

--- src/input_simulation/predict_input.jl
+++ src/input_simulation/predict_input.jl
@@ -1,6 +1,7 @@
 using Revise
 using Pkg
-Pkg.activate(raw"D:\Github\JutulDarcy.jl_shaowen")
+#Pkg.activate(raw"D:\Github\JutulDarcy.jl_shaowen") #Desktop at home
+Pkg.activate(raw"C:\\Users\\Shaow\\Documents\\GitHub\\JutulDarcy.jl_shaowen") # Dell laptop
 using JutulDarcy
 
 #simulate_mrst_case("C:/Users/Shaow/AppData/Local/Temp/qfs_wo", write_mrst = true, info_level=-1, ascii_terminal = true)
